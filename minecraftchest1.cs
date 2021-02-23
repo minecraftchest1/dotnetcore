ad5ff351f410c0fd33c48d17931a57b74431c922
Wyatt Jackson <minecraftchest1@outlook.com>
2021-02-23 17:17:51 +0000

    Create input method.

--- minecraftchest1.cs
+++ minecraftchest1.cs
@@ -9,5 +9,11 @@ namespace Minecraftchest1
             Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
         }
+        public static string Input(string prompt)
+        {
+            Console.Write(prompt);
+            string input = Console.ReadLine();
+            return input;
+        }
     }
 }
